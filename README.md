58213b01a9626e4580985d426b12a8ae72fa06ab
Ayaan Kazerouni <ayaank@calpoly.edu>
2021-01-26 18:14:04 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Playing around with Flask
+
+## Installing Flask
+[Instructions here.](https://flask.palletsprojects.com/en/1.1.x/installation/)
+
+## Quick Start Guide
+[Follow the official guide.](https://flask.palletsprojects.com/en/1.1.x/quickstart/)
+
+## RESTful APIs
+[This is a very good article to give you some background on REST API design](https://docs.microsoft.com/en-us/azure/architecture/best-practices/api-design)
+
+[This is a good quick guide.](https://restfulapi.net/)
---
+++ sample_backend.py
@@ -0,0 +1,93 @@
+from flask import Flask
+from flask import request
+from flask import jsonify
+from flask_cors import CORS
+import random
+import string
+from model_mongodb import User
+
+
+app = Flask(__name__)
+
+#CORS stands for Cross Origin Requests.
+CORS(app) #Here we'll allow requests coming from any domain. Not recommended for production environment.
+
+users = { 
+    'users_list' :
+    [
+        {  
+            'id' : 'xyz789',
+            'name' : 'Charlie',
+            'job': 'Janitor',
+        },
+        {
+            'id' : 'abc123',            
+            'name': 'Mac',
+            'job': 'Bouncer',
+        },
+        {
+            'id' : 'ppp222',            
+            'name': 'Mac',
+            'job': 'Professor',
+        },        
+        {
+            'id' : 'yat999',            
+            'name': 'Dee',
+            'job': 'Aspring actress',
+        },
+        {
+             'id' : 'zap555',           
+            'name': 'Dennis',
+            'job': 'Bartender',
+        }
+    ]
+}
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/users', methods=['GET', 'POST'])
+def get_users():
+    if request.method == 'GET':
+        search_username = request.args.get('name')
+        search_job = request.args.get('job')
+        if search_username and search_job:
+            # TODO: Replace with database access
+            result = find_users_by_name_job(search_username, search_job)  
+        elif search_username:
+            # using list shorthand for filtering the list.
+            # TODO: Replace with database access
+            result = [user for user in users['users_list'] if user['name'] == search_username]
+        else:
+            result = User().find_all()
+        return {"users_list": result}
+    elif request.method == 'POST':
+        userToAdd = request.get_json() # no need to generate an id ourselves
+        newUser = User(userToAdd)
+        newUser.save() # pymongo gives the record an "_id" field automatically
+        resp = jsonify(newUser), 201
+        return resp
+
+@app.route('/users/<id>', methods=['GET', 'DELETE'])
+def get_user(id):
+    if request.method == 'GET':
+        user = User({"_id":id})
+        if user.reload() :
+            return user
+        else :
+            return jsonify({"error": "User not found"}), 404
+    elif request.method == 'DELETE':
+        user = User({"_id":id})
+        resp = user.remove()
+
+        # TODO: Check the resp object if the removal was successful or not.
+        # Return a 404 status code if it was not successful
+        return {}, 204
+
+def find_users_by_name_job(name, job):
+    subdict = {'users_list' : []}
+    for user in users['users_list']:
+        if user['name'] == name and user['job'] == job:
+            subdict['users_list'].append(user)
+    return subdict  
