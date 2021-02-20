088d80f0d5e169644dfa9fda58b6fb6899e9fd18
Ryan Shintaku <ryanshintaku@ryans-mbp.lan>
2021-02-20 12:40:36 -0800

    travis test

--- sample_backend.py
+++ sample_backend.py
@@ -38,7 +38,7 @@ users = {
 
 @app.route('/')
 def hello_world():
-    return 'Hello world!'
+    return 'Hello world! Test Travis-CI'
 
 @app.route('/users', methods=['GET', 'POST'])
 def get_users():
